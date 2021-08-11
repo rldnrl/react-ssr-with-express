892d65719a72b004030ef3afda08a44c4e08d7dc
enginecode-cpu <bassmatics1002@gmail.com>
2021-08-11 22:44:19 +0900

    chore: remove css file

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom'
-import './index.css';
 import App from './App';
 import { store } from './store';
 import { Provider } from 'react-redux';
