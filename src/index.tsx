a962282b35ce0d82521c42abaf4c28d8ef20fa57
enginecode-cpu <bassmatics1002@gmail.com>
2021-08-17 23:52:12 +0900

    feat: apply hydrate in production

--- src/index.tsx
+++ src/index.tsx
@@ -7,24 +7,36 @@ import { Provider } from 'react-redux';
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { Hydrate } from 'react-query/hydration'
 import * as serviceWorker from './serviceWorker';
+import { loadableReady } from '@loadable/component';
 
 const dehydratedState = window.__REACT_QUERY_STATE__
 const queryClient = new QueryClient()
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <QueryClientProvider client={queryClient}>
-        <Hydrate state={dehydratedState}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </Hydrate>
-      </QueryClientProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const Root = () => {
+  return (
+    <React.StrictMode>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
+          <Hydrate state={dehydratedState}>
+            <BrowserRouter>
+              <App />
+            </BrowserRouter>
+          </Hydrate>
+        </QueryClientProvider>
+      </Provider>
+    </React.StrictMode>
+  )
+}
+
+const root = document.getElementById('root')
+
+if (process.env.NODE_ENV === "production") {
+  loadableReady(() => {
+    ReactDOM.hydrate(<Root />, root)
+  })
+} else {
+  ReactDOM.render(<Root />, root)
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
